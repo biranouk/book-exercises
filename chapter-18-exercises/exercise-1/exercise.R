2941bcffaa3870e4e7fe8463199b9df4f64af777
biranouk <biranouk@uw.edu>
2020-02-13 19:32:28 -0800

    Week 6 exercises complete

--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -8,9 +8,13 @@ library("jsonlite")
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
 
+setwd("C:/Users/brian/Desktop/INFO201/a5-data-report-biranouk/")
+source("apikey.R")
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
 
+movie_name <- "Parasite"
+query_param <- list("api_key" = tmdb_key, "movie_name" = movie_name)
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -21,10 +25,16 @@ library("jsonlite")
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
 
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+endpoint <- "reviews/search.json"
+uri <- paste0(base_uri, endpoint)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
 
+response <- GET(uri, query = query_param)
+body <- content(response, "text", encoding = "UTF-8")
+data <- fromJSON(body)
 
 # What kind of data structure did this produce? A data frame? A list?
 
